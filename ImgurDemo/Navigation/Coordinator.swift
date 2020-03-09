07065a21e8d25481608ae16a79a147820594e7c9
Adrian Kwiatkowski <kwiatkowski.adrian@icloud.com>
2020-03-09 21:27:25 +0100

    add gitignore

---
+++ ImgurDemo/Navigation/Coordinator.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinator.swift
+//  ImgurDemo
+//
+//  Created by Adrian Kwiatkowski 2 on 09/03/2020.
+//  Copyright © 2020 Adrian Kwiatkowski. All rights reserved.
+//
+
+import Foundation
---
+++ ImgurDemo/Navigation/MainCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  MainCoordinator.swift
+//  ImgurDemo
+//
+//  Created by Adrian Kwiatkowski 2 on 09/03/2020.
+//  Copyright © 2020 Adrian Kwiatkowski. All rights reserved.
+//
+
+import Foundation
